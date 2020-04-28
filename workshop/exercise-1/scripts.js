29f3e535db466610b62eea958a88e7b5cfc9b636
Mae <maeberris@outlook.com>
2020-04-27 20:36:00 -0400

    Exercise 1 start

--- workshop/exercise-1/scripts.js
+++ workshop/exercise-1/scripts.js
@@ -1,1 +1,10 @@
-// Add your code here!
+const main = document.createELement('main')
+
+document.body.appendChild(main)
+
+const h1 = document.createElement('h1')
+
+document.body.appendChild(h1)
+
+h1.innerText("The best How I Met Your Mother episode (according to fans)")
+

86c4d15e0f573a48708c0aa5dd81326cac99cacc
Andres Rueda <garo99@gmail.com>
2020-04-30 07:47:29 -0400

    v2

--- workshop/exercise-3-challenge/stopApp.js
+++ workshop/exercise-3-challenge/stopApp.js
@@ -1,28 +1,35 @@
-const stopwatchDiv = document.querySelector('#stopwatch');
-
-
+const STOPWATCHDIV = document.querySelector('#stopwatch');
+const MARKDIV = document.querySelector('#mark');
 
 function SecondsToTime(sec) {
-  let minutes = "00";
-  let seconds = "00";
-  let milisec = "00";
+  let minutes = '00';
+  let seconds = '00';
+  let milisec = '00';
   milisec = sec.toString();
-  milisec = milisec[milisec.length - 3] + "0";
+  milisec = milisec[milisec.length - 3] + '0';
   seconds = Math.round(sec / 1000);
   if (seconds >= 60) {
-    seconds = Math.round((seconds / 60) % 1 * 60);
+    seconds = Math.round(((seconds / 60) % 1) * 60);
   }
   seconds = seconds.toString();
   if (seconds.length === 1) {
-    seconds = "0" + seconds;
+    seconds = '0' + seconds;
   }
 
-  minutes = Math.floor(((sec / 1000) / 60) + 1 / 120)
+  minutes = Math.floor(sec / 1000 / 60 + 1 / 120);
   minutes = minutes.toString();
   if (minutes.length === 1) {
-    minutes = "0" + minutes;
+    minutes = '0' + minutes;
   }
-  return `${minutes}: ${seconds}: ${milisec} `
+  return `${minutes}: ${seconds}: ${milisec} `;
 }
+let stopWatch = parseInt(localStorage.getItem('time'));
+MARKDIV.innerHTML = SecondsToTime(stopWatch);
+// console.log('1', stopWatch);
 
-stopwatchDiv.innerHTML = SecondsToTime(localStorage.getItem("time"));
+setInterval(function () {
+  localStorage.setItem('time', stopWatch);
+  STOPWATCHDIV.innerHTML = SecondsToTime(stopWatch);
+  stopWatch += 100;
+  // console.log('2.', stopWatch);
+}, 100);

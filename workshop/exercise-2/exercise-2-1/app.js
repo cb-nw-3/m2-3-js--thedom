75958b4979096a4cb7f702721d362dd08f8dc3e5
TheMadPig <49966112+TheMadPig@users.noreply.github.com>
2020-04-30 19:34:49 -0400

    Exercise 2 with challenges

--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -27,7 +27,7 @@ head.appendChild(stylesheet);
 let racers = [];
 
 // loop to push three different frogs in racers array
-while (racers.length !== 3) {
+while (racers.length !== FROGS) {
   // generate random number between 0 and 4
   let randomFrog = Math.floor(Math.random() * frogStable.length);
   // statement to verify that frogs dont repeat
@@ -72,7 +72,7 @@ for (let i = 0; i < FROGS; i++) {
   // set lane id
   spanTitle.id = `lane-${i + 1}`;
   // set lane innerText
-  spanTitle.innerText = ` lane ${i + 1} : `;
+  spanTitle.innerText = ` Lane ${i + 1} : `;
   // append span title to h1
   h1.appendChild(spanTitle);
   // append span progress to span title
@@ -112,20 +112,54 @@ function racingFrog(frogObjFromRacersArr, i) {
       spanProgress.innerText = divFrog.style.left;
       // push each frog in the winners array
       winnersArray.push(frogObjFromRacersArr);
+      // get podium element
+      let gold = document.querySelector('.gold');
+      let silver = document.querySelector('.silver');
+      let bronze = document.querySelector('.bronze');
+      // log frgo names and numbers on podium
+      if (winnersArray.length === 1) {
+      gold.innerText = `${winnersArray[0].name} #${winnersArray[0].number}`;
+      } else if (winnersArray.length === 2) {
+      silver.innerText = `${winnersArray[1].name} #${winnersArray[1].number}`;
+      } else if (winnersArray.length === 3) {
+      bronze.innerText = `${winnersArray[2].name} #${winnersArray[2].number}`;
+      console.log(winnersArray);
+      }
     }
   }, randomDelay);
 }
 // initialize the winners array
 let winnersArray = [];
 // loop through racers array to activate the race
-for (let i = 0; i < racers.length; i++) {
+for (let i = 0; i < FROGS; i++) {
   racingFrog(racers[i], i);
 }
-// log the winners when length is 3
-if (winnersArray.length === 3) {
-  // log the winners
-  console.log(winnersArray);
-  let gold = document.querySelector('.gold');
-  let silver = document.querySelector('.silver');
-  let bronze = document.querySelector('.bronze');
+// select button for new race
+let buttonNewRace = document.querySelector('.newRace');
+// function for a new race
+buttonNewRace.onclick = function() {
+  for (let i = 0; i < FROGS; i++) {
+    // select the frog
+    let divFrog = document.querySelector(`#lane-${i + 1} .frog`);
+    // select span in title to update progress
+    let spanProgress = document.querySelector(`#frog-${i + 1}`);
+    // change the left property in css to make the frog move
+    divFrog.style.left = '0%';
+    // update progress in title
+    spanProgress.innerText = divFrog.style.left;
+    // get podium element
+    let gold = document.querySelector('.gold');
+    let silver = document.querySelector('.silver');
+    let bronze = document.querySelector('.bronze');
+    // remove previous winners
+    gold.innerText = ``;
+    silver.innerText = ``;
+    bronze.innerText = ``;
+    // empty winnersArray
+    winnersArray = [];
+    // reset racers progress
+    racers[i].progress = 0;
+    // recall the function to make frogs race
+    racingFrog(racers[i], i);
+  }
 }

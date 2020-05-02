6297056a06e9db97ad95a1d5869ed843b2a8df34
rogerlam <rogerlam@wylco.org>
2020-05-02 10:31:45 -0400

    finish to exercise 2

--- workshop/exercise-1/scripts.js
+++ workshop/exercise-1/scripts.js
@@ -1,1 +1,71 @@
 // Add your code here!
+
+let main = document.createElement("main");
+
+document.body.appendChild(main);
+
+// header
+
+let topHeader = document.createElement("h1");
+topHeader.innerText = "The best How I Met Your Mother episode (according to fans)";
+
+document.querySelector("main").appendChild(topHeader);
+
+// main paragraph
+
+let mainPara = document.createElement("p");
+mainPara.innerText = "As impossible as it seems to name just 1 of the 208 legendary episodes as the “best”, the fans have spoken and the highest rated episode is… wait for it…"
+
+document.querySelector("main").appendChild(mainPara);
+
+// h2
+
+let secondHeader = document.createElement("h2");
+secondHeader.innerText = "The Slap Bet (Season 2, Episode 9)";
+
+document.querySelector("main").appendChild(secondHeader);
+
+// imdb rating
+
+let imdb = document.createElement("p");
+imdb.innerText = "IMDB Rating: 9.5";
+
+document.querySelector("main").appendChild(imdb);
+
+// img
+
+let sparkly = document.createElement("img");
+sparkly.setAttribute("src", "images/robin-sparkles.jpg");
+sparkly.setAttribute("alt", "Robin Sparkles");
+
+document.querySelector("main").appendChild(sparkly);
+
+// summary 1
+
+let summaryOne = document.createElement("p");
+summaryOne.innerText = "In this episode, Ted is learning a few secrets about Robin, namely that she has an unexplained aversion to the mall. Robin refuses to tell Ted and the others why she won’t go to the mall, so the gang forms their own theories. Marshall believes it’s because she got married in a mall and is still married. Barney believes it has something to do with Robin having performed in a porn video. The two of them make a slap bet with each other: whoever is right gets to slap the other across the face as hard as he can. Lily is named the Slap Bet Commissioner, as long as she promises to be unbiased.";
+
+document.querySelector("main").appendChild(summaryOne);
+
+// summary 2
+
+let summaryTwo = document.createElement("p");
+summaryTwo.innerText = "In the end, Robin’s big secret was revealed- she was a Canadian, teen pop sensation named Robin Sparkles. Barney finds the music video for her hit single “Lets Go To The Mall”. The slap bet takes a few turns throughout the episode, but ends with Marshall having the right to slap Barney 5 times- available for all of eternity (horrible call Barney).";
+
+document.querySelector("main").appendChild(summaryTwo);
+
+// anchor guy
+
+let anchor = document.createElement("a");
+anchor.setAttribute("href", "https://himym2017.wordpress.com/2017/03/25/the-best-himym-episode-according-to-fans/");
+anchor.innerText = "Source";
+
+document.querySelector("main").appendChild(anchor);
+
+// style
+
+let styleLink = document.createElement("link");
+styleLink.setAttribute("rel", "stylesheet");
+styleLink.setAttribute("href", "styles.css");
+
+document.head.appendChild(styleLink);
--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -1,5 +1,5 @@
 // Preset values
-const FROGS = 3;
+const FROGS = 5;
 
 // 1. Create for loop that makes use of FROGS to know how many lanes to create.
 
@@ -8,3 +8,68 @@ const FROGS = 3;
 // 3. Create span and add it to the li
 
 // 4. Assign an id to each lane
+
+for (let i = 1; i <= 3; i++) {
+    let lane = document.createElement("li");
+    let laneId = "lane-" + i;
+    lane.setAttribute("id", laneId);
+    lane.innerHTML = "<span>" + i + "</span>";
+    document.querySelector("ol").appendChild(lane);
+}
+
+let racers = [];
+
+for (let i = 1; i <= 3; i++) {
+    let pickNum = Math.floor(Math.random() * FROGS);
+    if (racers.includes(frogStable[pickNum]) != true) {
+        racers.push(frogStable[pickNum])
+    } else {
+        i--;
+    }
+}
+
+for (let i = 0; i < 3; i++) {
+    let frogContender = document.createElement("span");
+    frogContender.classList.add("frog");
+    frogContender.style.background = racers[i]["color"];
+    frogContender.setAttribute("id", racers[i]["name"]);
+    racers[i]["progress"] = 0;
+    frogContender.innerText = racers[i]["name"] + " / " + racers[i]["number"] + " / " + racers[i]["progress"];
+    document.querySelectorAll("li")[i].appendChild(frogContender);
+}
+
+let ranking = [];
+
+function racingFrog(frogRacer) {
+    let raceProgress = setInterval(function () {
+        if (frogRacer["progress"] < 100) {
+            frogRacer["progress"] += Math.floor(Math.random() * 50);
+            if (frogRacer["progress"] > 100) {
+                document.querySelector("#" + frogRacer["name"]).style.left = 100 + "%";
+            } else {
+                document.querySelector("#" + frogRacer["name"]).style.left = frogRacer["progress"] + "%";
+            }
+        } else {
+            ranking.push(frogRacer);
+            clearInterval(raceProgress);
+        }
+    }, Math.random() * 1000);
+};
+
+for (let i = 0; i < 3; i++) {
+    racingFrog(racers[i]);
+}
+
+function finishLine() {
+    console.log("Congratulations to all the contestants!");
+    console.log("First place: " + ranking[0]["name"]);
+    console.log("Second place: " + ranking[1]["name"]);
+    console.log("Third place: " + ranking[2]["name"]);
+}
+
+let finalRank = setInterval(function () {
+    if (ranking.length === 3) {
+        finishLine();
+        clearInterval(finalRank);
+    }
+}, 5)

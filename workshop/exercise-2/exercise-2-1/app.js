9288169c7fb09c5831823a7c4bceef4ab18dbd66
jenny ma <jenny_lau_8@hotmail.com>
2020-05-02 01:11:03 -0400

    F

--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -33,24 +33,83 @@ console.log(racers);
 
 
 //Exercise 2.3 - Line 'em up!
-racers.forEach(function (racersParameter1, racersIndex) {
+racers.forEach(function (racersPlayer, racersIndex) {
   //creating at least one element inside the lane `li`
   const threeFrogs = document.createElement('span');
   //show the name/number of the frog in the lane; put racer.name is I want to show name
-  threeFrogs.innerText = `${racersParameter1.number}`;
+  threeFrogs.innerText = `${racersPlayer.number}`;
   //Add a class of `frog`
   threeFrogs.classList.add('frog');
   //set the background color from frogStable element color
-  threeFrogs.style.background = racersParameter1.color;
-  //append frogs to each lane that has an id of 'lane-1' etc.
+  threeFrogs.style.background = racersPlayer.color;
+  //append frogs to each lane that has an id of 'lane-1', 'lane-2', 'lane-3'
   document.getElementById(`lane-${racersIndex + 1}`).appendChild(threeFrogs);
 
+    // Set the frog's progress to 0 inside the racers array
+  racers[racersIndex].progress = 0;
+  //inside its object
+  racers[racersIndex].lane = `lane-${racersIndex + 1}`;
   
-  // Set the frog's progress to 0 inside its object, inside of the racers array.
-  
+  //create 3rd span for progress
+  const frogProgress = document.createElement('span');
+  frogProgress.racersIndex = racers[racersIndex].name;
+  document.getElementById(`lane-${racersIndex + 1}`).appendChild(frogProgress);
 })
+
 //added stylesheet in html at this point
 const styleSheetLink = document.createElement('link');
 styleSheetLink.rel = "stylesheet";
-
 document.head.appendChild(styleSheetLink);
+
+//Exercise 2.4 - Make 'em hop!
+let ranking = [];
+
+function racingFrog(racersPlayer) {
+  let progress = racersPlayer.progress;
+  const trackLength = track.offsetWidth;
+
+  //set a random hop; math.random() is a number between zero and 1
+  //need to *100 before / by trackLength
+  //the hop will be very slow without *99; just a random number
+  const hopLength = ((Math.random() * 100) / trackLength)*99;
+
+  const stop = setInterval(function () {
+    // increase the progress by the random hopLength
+    //remember that progress was set to 0
+    progress += hopLength;
+    // Check if progress is more than 100%. If so, set it to 100
+    if (progress > 100) {
+      progress = 100;
+      console.log('Bravo', racersPlayer.name, '! You finished the race !');
+      clearInterval(stop);
+
+      // (for Exercise 2.5)
+      ranking.push(racersPlayer);
+    }
+
+    // Moving the frog on the screen
+    //css .style.left so that the frog start on the left?
+    //Math.random()*1000 to add a variable delay
+    document.querySelector(`#${racersPlayer.lane} .frog`).style.left = `${progress}%`;
+  }, Math.random() * 1000);
+}
+
+// startRace
+racers.forEach(function (racersPlayer) {
+  // call this function once for each racer.
+  racingFrog(racersPlayer);
+});
+
+//Exercise 2.5 - And the Winner is
+let rankingInterval = setInterval(function() {
+  if (racers.length === ranking.length) {
+    console.log('All going to the semifinals against the Turtles!');
+    ranking.forEach(function(racersPlayer, racersIndex) {
+      const announcement = document.createElement('p');
+      announcement.innerText = `Top ${racersIndex + 1}: ${racersPlayer.name}`;
+      document.body.appendChild(announcement);
+    })
+    console.log(ranking);
+    clearInterval(rankingInterval);
+  }
+}, 200);

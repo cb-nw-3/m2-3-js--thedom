fbfe9bec60e5931e408cb1e365cb65e499db83aa
kolbymckeown <mckeownkolby@gmail.com>
2020-05-02 15:18:13 -0400

    Workshop Complete

--- workshop/exercise-2/assets/frogStable.js
+++ workshop/exercise-2/assets/frogStable.js
@@ -3,25 +3,30 @@ const frogStable = [
     name: 'Hopper',
     color: 'blue',
     number: '32',
+    id: '0',
   },
   {
     name: 'Legs',
     color: 'red',
     number: '79',
+    id: '1',
   },
   {
     name: 'Bouncer',
     color: 'brown',
     number: '6',
+    id: '2',
   },
   {
     name: 'Springs',
     color: 'purple',
     number: '48',
+    id: '3',
   },
   {
     name: 'Fred',
     color: 'black',
     number: '8',
+    id: '4',
   },
 ];

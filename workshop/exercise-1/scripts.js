945454306b11a88d8cb2f4e6eb4a6c7b2c9b23e2
Przemek Piwek <przemekpiwek@gmail.com>
2020-05-02 07:57:37 -0600

    completed exercises

--- workshop/exercise-1/scripts.js
+++ workshop/exercise-1/scripts.js
@@ -1,1 +1,39 @@
 // Add your code here!
+
+let body = document.querySelector('body');
+let main = document.createElement('main');
+let titleHeader = document.createElement('h1');
+let titlePara = document.createElement('p');
+let episodeHeader = document.createElement('h2');
+let episodeSubTitle = document.createElement('p');
+let episodePara1 = document.createElement('p');
+let episodePara2 = document.createElement('p');
+let episodeImg = document.createElement('img');
+let source = document.createElement('a');
+
+body.appendChild(main);
+main.appendChild(titleHeader);
+main.appendChild(titlePara);
+main.appendChild(episodeHeader);
+main.appendChild(episodeSubTitle);
+main.appendChild(episodeImg);
+main.appendChild(episodePara1);
+main.appendChild(episodePara2);
+main.appendChild(source);
+
+
+titleHeader.innerText = 'The best How I Met Your Mother episode (according to fans)';
+titlePara.innerText = 'As impossible as it seems to name just 1 of the 208 legendary episodes as the “best”, the fans have spoken and the highest rated episode is… wait for it…';
+episodeHeader.innerText = 'The Slap Bet (Season 2, Episode 9)';
+episodeSubTitle.innerText = 'IMDB Rating: 9.5';
+episodeImg.src = "images/robin-sparkles.jpg";
+episodeImg.alt = "Robin Sparkles";
+episodePara1.innerText = 'In this episode, Ted is learning a few secrets about Robin, namely that she has an unexplained aversion to the mall. Robin refuses to tell Ted and the others why she won’t go to the mall, so the gang forms their own theories. Marshall believes it’s because she got married in a mall and is still married. Barney believes it has something to do with Robin having performed in a porn video. The two of them make a slap bet with each other: whoever is right gets to slap the other across the face as hard as he can. Lily is named the Slap Bet Commissioner, as long as she promises to be unbiased.'
+episodePara2.innerText = 'In the end, Robin’s big secret was revealed- she was a Canadian, teen pop sensation named Robin Sparkles. Barney finds the music video for her hit single “Lets Go To The Mall”. The slap bet takes a few turns throughout the episode, but ends with Marshall having the right to slap Barney 5 times- available for all of eternity (horrible call Barney).'
+source.innerText = "Source";
+source.href = "https://himym2017.wordpress.com/2017/03/25/the-best-himym-episode-according-to-fans/"
+
+const stylesheet = document.createElement('link');
+stylesheet.rel = 'stylesheet';
+stylesheet.href = 'styles.css';
+document.body.appendChild(stylesheet);
--- workshop/exercise-2/assets/frogStable.js
+++ workshop/exercise-2/assets/frogStable.js
@@ -25,3 +25,70 @@ const frogStable = [
     number: '8',
   },
 ];
+
+
+//2.2
+const racers = [];
+while(racers.length < FROGS){
+  let j = frogStable.length;
+  let i = parseInt(Math.random()*j);
+  racers.push(frogStable[i]);
+  frogStable.splice(i,1);
+}
+
+//2.3
+racers.forEach((frogObj,index) => { 
+//assign variable to each lane number 
+  let lane = document.querySelector(`#lane-${index+1}`);
+  let frogContainer = document.createElement('div');
+  let frogNumber = document.createTextNode(`${frogObj.number}`);
+
+  frogContainer.appendChild(frogNumber);
+  frogContainer.setAttribute('style',`color:${frogObj.color}`);
+  frogContainer.classList.add('frog');
+  lane.appendChild(frogContainer);
+//add progress key to each racer and set to 0
+  frogObj.progress = 0;
+  frogObj.lane = `lane-${index+1}`;
+})
+
+//2.4
+
+//racingFrog function with one parameter Frog
+
+const ranking = [];
+
+racingFrog = (frog) => {
+  let laneLength = document.getElementById("lane-1").offsetWidth;
+  let frogDiv = document.querySelector(`#${frog.lane} > div`);
+
+  let intID = setInterval(() => {
+    let hop = (Math.random()*laneLength)/laneLength * 100;
+
+    frog.progress += hop;
+    frogDiv.style.left = `${frog.progress}%`;
+  
+    if(frog.progress > 100){
+      console.log(`${frog.number} has finished the race`)
+      ranking.push(frog.number);
+      frogDiv.style.left = `100%`;
+      clearInterval(intID);
+    }
+
+    if(ranking.length === FROGS){
+      let results = document.querySelector('p');
+      let str = '';
+      for(let i = 1; i <= FROGS; i++){
+        str += `${i} place: Number ${ranking[i-1]} \n`;
+      }
+      results.innerText = str;
+    }
+  }, 1000);
+}
+
+for(let i=0; i <racers.length; i++){
+  racingFrog(racers[i]);
+}
+
+
+

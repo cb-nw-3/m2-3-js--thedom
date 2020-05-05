ef576ee368ad1eb5eb21eb02359b4d5950cefe61
anwesh-sarkar <anwesh.sarkar@gmail.com>
2020-05-04 20:08:33 -0400

    Exercise 1 & 2 - sorry no challenges

--- workshop/exercise-1/scripts.js
+++ workshop/exercise-1/scripts.js
@@ -1,1 +1,46 @@
-// Add your code here!
+const main = document.createElement("main");
+document.body.appendChild(main);
+
+const heading1 = document.createElement("h1");
+heading1.innerText =
+  "The best How I Met Your Mother episode (according to fans)";
+main.appendChild(heading1);
+
+const paragraph1 = document.createElement("p");
+paragraph1.innerText =
+  "As impossible as it seems to name just 1 of the 208 legendary episodes as the “best”, the fans have spoken and the highest rated episode is… wait for it…";
+main.appendChild(paragraph1);
+
+const heading2 = document.createElement("h2");
+heading2.innerText = "The Slap Bet (Season 2, Episode 9)";
+main.appendChild(heading2);
+
+const IMDBrating = document.createElement("p");
+IMDBrating.innerText = "IMDB Rating: 9.5";
+main.appendChild(IMDBrating);
+
+const image = document.createElement("img");
+image.src = "images/robin-sparkles.jpg";
+image.alt = "Robin Sparkles";
+main.appendChild(image);
+
+const paragraph2 = document.createElement("p");
+paragraph2.innerText =
+  "In this episode, Ted is learning a few secrets about Robin, namely that she has an unexplained aversion to the mall. Robin refuses to tell Ted and the others why she won’t go to the mall, so the gang forms their own theories. Marshall believes it’s because she got married in a mall and is still married. Barney believes it has something to do with Robin having performed in a porn video. The two of them make a slap bet with each other: whoever is right gets to slap the other across the face as hard as he can. Lily is named the Slap Bet Commissioner, as long as she promises to be unbiased.";
+main.appendChild(paragraph2);
+
+const paragraph3 = document.createElement("p");
+paragraph3.innerText =
+  "In the end, Robin’s big secret was revealed- she was a Canadian, teen pop sensation named Robin Sparkles. Barney finds the music video for her hit single “Lets Go To The Mall”. The slap bet takes a few turns throughout the episode, but ends with Marshall having the right to slap Barney 5 times- available for all of eternity (horrible call Barney).";
+main.appendChild(paragraph3);
+
+const source = document.createElement("a");
+source.href =
+  "https://himym2017.wordpress.com/2017/03/25/the-best-himym-episode-according-to-fans/";
+source.innerText = "Source";
+main.appendChild(source);
+
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "styles.css";
+document.head.appendChild(link);
--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -1,10 +1,71 @@
-// Preset values
+// Exercise 2.1
 const FROGS = 3;
 
-// 1. Create for loop that makes use of FROGS to know how many lanes to create.
+const track = document.querySelector("#track");
+document.body.appendChild(track);
+for (let count = 1; count <= FROGS; count++) {
+  let lane = document.createElement("li");
+  track.appendChild(lane);
+  let laneNumber = document.createElement("span");
+  laneNumber.innerText = count;
+  lane.appendChild(laneNumber);
 
-// 2. Create li
+  lane.id = "lane-" + count;
+}
 
-// 3. Create span and add it to the li
+// Exercise 2.2
+let racers = [];
 
-// 4. Assign an id to each lane
+for (let i = 1; i <= FROGS; i++) {
+  racers.push(frogStable[i]);
+}
+
+console.log(racers);
+
+// Exercise 2.3
+for (let i = 0; i < FROGS; i++) {
+  let frogRacer = document.createElement("span");
+  frogRacer.innerText = racers[i]["number"];
+  frogRacer.classList.add("frog");
+  frogRacer.style.color = racers[i]["color"];
+  document.querySelector(`#lane-${i + 1}`).appendChild(frogRacer);
+
+  racers[i].progress = 0;
+  racers[i].lane = "lane-" + (i + 1);
+}
+
+//Exercise 2.5
+let ranking = [];
+
+// Exercise 2.4
+function racingFrog(frogObj) {
+  let trackLength = track.offsetWidth;
+  let hop = Math.round(((Math.random() * 100) / trackLength) * 100);
+
+  let progress = frogObj["progress"];
+
+  let hopFrog = setInterval(function () {
+    progress = progress + hop;
+    if (progress >= 100) {
+      progress = 100;
+      console.log(frogObj["name"], " has finished");
+      clearInterval(hopFrog);
+      ranking.push(frogObj);
+    }
+    document.querySelector(
+      `#${frogObj.lane} .frog`
+    ).style.left = `${progress}%`;
+  }, Math.random() * 1000);
+}
+
+racers.forEach(function (racer) {
+  racingFrog(racer);
+});
+
+// Exercise 2.5
+let raceFinish = setInterval(function (racer) {
+  if (ranking.length === racers.length) {
+    console.log(ranking[0].name, ranking[1].name, ranking[2].name);
+    clearInterval(raceFinish);
+  }
+}, 5);

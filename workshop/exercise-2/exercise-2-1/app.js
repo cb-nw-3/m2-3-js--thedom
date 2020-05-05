c94cf4b47f69885dc08724c8a85e586c755bd07c
bernard love constant <top_game29@hotmail.com>
2020-05-04 22:11:28 -0400

    m2-3-js done

--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -1,10 +1,16 @@
 // Preset values
 const FROGS = 3;
+const track = document.getElementById("track");
 
 // 1. Create for loop that makes use of FROGS to know how many lanes to create.
-
-// 2. Create li
-
+for (let numLane = ; numLane <= FROGS; numLane++) {
+  // 2. Create li
+let lane = document.createElement("li");
+track.appendChild(lane);
 // 3. Create span and add it to the li
-
+let span1 = document.createElement('span');
+span1.innerText = 'numLane';
+lane.appendChild(span1);
 // 4. Assign an id to each lane
+lane.id = `frog-${numLane}`;
+}

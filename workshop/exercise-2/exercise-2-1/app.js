577aabd97066e1e9947c809087f31fc94debd5b9
Nathalie Benarroch <nathaliebenarroch@Nathalies-MacBook-Pro.local>
2020-05-06 20:11:44 -0400

    commiting to master

--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -1,48 +1,47 @@
 // Preset values
 const FROGS = 3;
 
+let RACERS = [];
+
+for (i = 0; i < FROGS; i++) {
+  let frog = frogStable[i];
+  RACERS.push(frog);
+}
+console.log(RACERS);
+
 function addStyleTag() {
-  let style = document.createElement("link");
+  let styleTag = document.createElement("link");
   styleTag.rel = "stylesheet";
-  styleTag.href = "styles.css";
+  styleTag.href = "../assets/styles.css";
   return styleTag;
 }
 
 let ol = document.querySelector("#track");
 
 // 4. Assign an id to each lane
-function theLaneOne() {
-  let lane = document.createElement("li");
-  myDiv.classlist.add("track");
-  return lane;
-}
 
-function theLaneTwo() {
+function laneGenerator(laneNumber) {
   let lane = document.createElement("li");
-  myDiv.classlist.add("track li");
-  return lane;
-}
-
-function theLaneThree() {
-  let lane = document.createElement("li");
-  myDiv.classlist.add("track li:last-child");
+  lane.setAttribute("id", `lane-${laneNumber}`);
+  let span = document.createElement("span");
+  span.innerText = laneNumber;
+  lane.appendChild(span);
   return lane;
 }
 
 // 1. Create for loop that makes use of FROGS to know how many lanes to create.
-// 3. Create span and add it to the li
 
 // appending all children nodes or lanes to main parent lane
 function theLane() {
-  let allTheLanes = document.createElement("ol");
-  allTheLanes.appendChild(theLaneOne("track"));
-  allTheLanes.appendChild(theLaneTwo("track li"));
-  allTheLanes.appendChild(theLaneThree("track li:last-child"));
+  let allTheLanes = document.querySelector("ol");
+  for (let i = 0; i < FROGS; i++) {
+    allTheLanes.appendChild(laneGenerator(i + 1));
+  }
 }
 
 // appending style tag and ol
 document.head.appendChild(addStyleTag());
-ol.appendChild(theLane());
+theLane();
 
 // function theRating() {
 //     let therate = document.createElement("p");
